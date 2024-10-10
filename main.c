cd5e15e586cb1ecaf1b135cccdc4026945f565a7
Shresth Prasad <shresthprasad7@gmail.com>
2024-10-11 00:58:24 +0530

    add definition for some tetrino shapes

--- main.c
+++ main.c
@@ -4,6 +4,42 @@
 #define HEIGHT 22
 #define VISIBLE_HEIGHT 20
 
+#define construct_tetrino(data, side) {data, side}
+
+/*
+ * Reprsents the shape of the tetrino
+ * Here, `data` is square matrix
+ * each side of the matrix is of lenght `side`
+ */
+typedef struct {
+    const uint8_t *data;
+    const int32_t side;
+} tetrino;
+
+const uint8_t TETRINO_1[] = {
+    0, 0, 0, 0,
+    1, 1, 1, 1,
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+};
+
+const uint8_t TETRINO_2[] = {
+    2, 2,
+    2, 2
+};
+ 
+const uint8_t TETRINO_3[] = {
+    0, 0, 0,
+    3, 3, 3,
+    0, 3, 0
+};
+
+const tetrino TETRINOS[] = {
+    construct_tetrino(TETRINO_1, 4),
+    construct_tetrino(TETRINO_2, 2),
+    construct_tetrino(TETRINO_3, 3),
+};
+
 /*
  * Struct representing state of each tetrino
  * tetrino_index selects one of 8 tetrinos
