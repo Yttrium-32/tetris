de0318b8bd29341158fe646b3bc63fa3990a9542
Shresth Prasad <shresthprasad7@gmail.com>
2024-10-12 02:28:46 +0530

    change all instances of tetrino to tetromino

--- src/main.c
+++ src/main.c
@@ -11,17 +11,17 @@
 #define HEIGHT 22
 #define VISIBLE_HEIGHT 20
 
-#define construct_tetrino(data, side) {data, side}
+#define construct_tetromino(data, side) {data, side}
 
 /*
- * Reprsents the shape of the tetrino
+ * Reprsents the shape of the tetromino
  * Here, `data` is square matrix
  * each side of the matrix is of lenght `side`
  */
 typedef struct {
     const uint8_t *data;
     const int32_t side;
-} tetrino;
+} tetromino;
 
 const uint8_t TETRINO_1[] = {
     0, 0, 0, 0,
@@ -41,10 +41,10 @@ const uint8_t TETRINO_3[] = {
     0, 3, 0
 };
 
-const tetrino TETRINOS[] = {
-    construct_tetrino(TETRINO_1, 4),
-    construct_tetrino(TETRINO_2, 2),
-    construct_tetrino(TETRINO_3, 3),
+const tetromino TETRINOS[] = {
+    construct_tetromino(TETRINO_1, 4),
+    construct_tetromino(TETRINO_2, 2),
+    construct_tetromino(TETRINO_3, 3),
 };
 
 typedef enum {
@@ -52,11 +52,11 @@ typedef enum {
 } game_phase;
 
 /*
- * Struct representing state of each tetrino
- * tetrino_index selects one of 8 tetrinos
+ * Struct representing state of each tetromino
+ * tetromino_index selects one of 8 tetrominos
  */
 typedef struct {
-    uint8_t tetrino_index;
+    uint8_t tetromino_index;
     int32_t offset_row;
     int32_t offset_col;
     int32_t rotation;
@@ -85,28 +85,28 @@ void matrix_set(uint8_t *values, int32_t width, int32_t row, int32_t col, uint8_
     values[index] = value;
 }
 
-uint8_t tetrino_get(const tetrino *tetrino, int32_t row, int32_t col, int32_t rotation) {
-    int32_t side = tetrino -> side;
+uint8_t tetromino_get(const tetromino *tetromino, int32_t row, int32_t col, int32_t rotation) {
+    int32_t side = tetromino -> side;
     switch (rotation) {
         case 0:
-            return tetrino -> data[row * side + col];
+            return tetromino -> data[row * side + col];
         case 1:
-            return tetrino -> data[(side - col - 1) * side + row];
+            return tetromino -> data[(side - col - 1) * side + row];
         case 2:
-            return tetrino -> data[(side - row - 1) * side + (side - col - 1)];
+            return tetromino -> data[(side - row - 1) * side + (side - col - 1)];
         case 3:
-            return tetrino -> data[col * side + (side - row - 1)];
+            return tetromino -> data[col * side + (side - row - 1)];
     }
     return 0;
 }
 
 bool check_piece_valid(const piece_state *piece, const uint8_t *board, int32_t width, int32_t height) {
-    const tetrino *tetrino = &TETRINOS[piece->tetrino_index];
-    assert(tetrino);
+    const tetromino *tetromino = &TETRINOS[piece->tetromino_index];
+    assert(tetromino);
 
     for (int32_t row = 0; row < height; ++row) {
         for (int32_t col = 0; col < width; ++col) {
-            uint8_t value = tetrino_get(tetrino, row, col, piece->rotation);
+            uint8_t value = tetromino_get(tetromino, row, col, piece->rotation);
             if (value > 0) {
                 int32_t board_row = piece->offset_row + row;
                 int32_t board_col = piece->offset_col + col;
